8cc9d6549d68216c93624bd679f52e9cdf27f3fe
Jordan Vastag <jvastag44@gmail.com>
2025-04-05 17:36:26 -0500

    fix: remove unsupported dependency

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,11 +1,9 @@
 import jwt
 from fastapi.security import OAuth2PasswordBearer
-from passlib.context import CryptContext
+import bcrypt
 from datetime import datetime, timedelta
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/token")
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 SECRET_KEY = "*v!zjWFbC&QLa2%UFTcerI9ydQNJ0J9E"
 ALGORITHM = "HS256"
@@ -13,11 +11,15 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 def get_password_hash(password: str) -> str:
-    return pwd_context.hash(password)
+    pwd_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password=pwd_bytes, salt=salt).decode("utf-8")
+    return hashed_password
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+    password_byte_enc = plain_password.encode('utf-8')
+    return bcrypt.checkpw(password = password_byte_enc , hashed_password = hashed_password)
 
 
 def create_access_token(*, data: dict, expires_delta: timedelta = None):
