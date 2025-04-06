f5aa0d8bd9dae32ea9b9263f1f83fc1f58fd3ee0
Jordan Vastag <jvastag44@gmail.com>
2025-04-06 16:31:29 -0500

    remove non-mvp startup steps

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,15 +12,6 @@ docker compose create --build --remove-orphans
 # Run containers
 docker compose start
 
-# Hack to wait for postgres container to be up before running alembic migrations
-sleep 5;
-
-# Run migrations
-docker compose run --rm backend alembic upgrade head
-
-# Create initial data
-docker compose run --rm backend python3 app/db/scripts/initial_data.py
-
 # TODO: configure this
 # Watch for file changes
 # docker compose watch
