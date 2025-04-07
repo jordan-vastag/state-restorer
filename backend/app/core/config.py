305d2365bae2ad774407d7d8126ccef5a372020a
Jordan Vastag <jvastag44@gmail.com>
2025-04-07 14:33:44 -0500

    update grayscale theme colors

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -9,8 +9,8 @@ FRONTEND_LOCAL_URL = "http://localhost:5173"
 DEFAULT_CELL_COLOR_THEME = "grayscale"
 CELL_COLOR_THEMES = {
     "grayscale": {
-        "colors": ["#ffffff", "#000000"],
-        "complements": [("#ffffff", "#000000")],
+        "colors": ["#f6f6f6", "#000000"],
+        "complements": [("#f6f6f6", "#000000")],
     },
     "basic": {
         "colors": ["#ff5757", "#46cc46", "#69abe1", "#ffc26d"],
