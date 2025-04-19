b6fd1238a8302fb51cca60f739e4ed604d3bef43
Jordan Vastag <jvastag44@gmail.com>
2025-04-19 11:15:40 -0500

    feat: add footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,8 +1,18 @@
-import { } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 
 function Footer() {
+  const year = new Date().getFullYear();
+  // const year = new Date().getFullYear();
+  // const year = new Date().getFullYear();
+  // const year = new Date().getFullYear();
+  // const year = new Date().getFullYear();
+  // const
+
   return (
     <>
+      <Flex alignItems="center" justifyContent="center" padding="5">
+        <div>© State Restorer {year}. All rights reserved.</div>
+      </Flex>
     </>
   );
 }
