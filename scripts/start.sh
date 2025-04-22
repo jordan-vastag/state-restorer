d53218f4dc36e743e5f1412de192037e4699eabb
Jordan Vastag <jvastag44@gmail.com>
2025-04-22 14:36:26 -0500

    fix: update scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-echo "Restarting..."
-
-echo ""
-
-echo "Stopping backend..."
-fuser -k 8000/tcp
-echo "Stopped."
+echo "Doing setup..."
 cd backend || exit 1
 if [ ! -d "venv" ]; then
     echo "Virtual environment not found."
@@ -20,21 +14,37 @@ if [ ! -d "venv" ]; then
     fi
     echo "Creating virtual environment..."
     python3 -m venv venv
+    echo "Virtual environment created."
+    echo "Activating virtual environment..."
+    . venv/bin/activate
+    echo "Virtual environment activated."
 fi
-. venv/bin/activate
-echo "Installing backend dependencies..."
-(sudo apt update && sudo apt install -y libpq-dev && pip install -r requirements.txt) || {
-    echo -e "\e[31m\nERROR: Failed to install backend dependencies.\e[0m"
-    exit 1
-}
-fastapi run app/main.py &
 
-echo ""
+if [ ! -f "venv/dependencies_installed_flag" ]; then
+    echo "Installing backend dependencies..."
+    echo "Checking if psycopg2 dependency (libpq-dev) is installed..."
+    if ! dpkg -l | grep -q libpq-dev; then
+        echo "libpq-dev is not installed. Installing..."
+        (sudo apt update && sudo apt install -y libpq-dev) || {
+            echo -e "\e[31m\nERROR: Failed to install libpq-dev.\e[0m"
+            exit 1
+        }
+    else
+        echo "libpq-dev is already installed."
+    fi
 
-echo "Stopping frontend..."
-fuser -k 5173/tcp
-echo "Stopped."
-cd ../frontend || exit 1
+    echo "Installing requirements..."
+    pip install -r requirements.txt || {
+        echo -e "\e[31m\nERROR: Failed to install requirements.\e[0m"
+        exit 1
+    }
+    touch venv/dependencies_installed_flag
+    echo "Requirements installed and flag file created."
+else
+    echo "Assuming backend dependencies are up to date."
+    echo "NOTE: if the API fails to start, delete the venv/dependencies_installed_flag file and rerun the script."
+fi
+
 if ! command -v node &>/dev/null; then
     echo "Node.js is not installed. Attempting to install using nvm..."
     if ! command -v nvm &>/dev/null; then
@@ -62,6 +72,26 @@ yarn || {
     echo -e "\e[31m\nERROR: Failed to install frontend dependencies.\e[0m]"
     exit 1
 }
+cd ..
+echo "Setup complete."
+
+echo "Starting app..."
+if lsof -i:8000 &>/dev/null; then
+    echo "Backend already running. Killing existing process..."
+    fuser -k 8000/tcp >/dev/null 2>&1
+    echo "Stopped."
+fi
+echo "Starting backend..."
+cd backend && fastapi run app/main.py &
 echo "Done."
+
+if lsof -i:5173 &>/dev/null; then
+    echo "Frontend already running. Killing existing process..."
+    fuser -k 5173/tcp >/dev/null 2>&1
+    echo "Stopped."
+fi
 echo "Starting frontend..."
-yarn run dev &
+cd frontend && npm run dev &
+echo "Done."
+
+echo "App started."
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo "Stopping backend"
-fuser -k 8000/tcp
+echo "Stopping backend..."
+fuser -k 8000/tcp >/dev/null 2>&1
+echo "Stopped."
 
-echo "Stopping frontend"
-fuser -k 5173/tcp
+echo "Stopping frontend..."
+fuser -k 5173/tcp >/dev/null 2>&1
+echo "Stopped."
