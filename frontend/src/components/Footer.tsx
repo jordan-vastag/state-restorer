8fd64f57b4f956177bb3105b48f4feccffc3bbcb
Jordan Vastag <jvastag44@gmail.com>
2025-09-23 15:05:37 -0500

    feat: add version to footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,23 +1,36 @@
-import { Box, Flex, Link } from "@chakra-ui/react";
+import { Box, Flex, Link, Text } from "@chakra-ui/react";
 
 function Footer() {
   const year = new Date().getFullYear();
+  const app_version = import.meta.env.VITE_APP_VERSION;
 
   return (
     <>
       {/* TODO: use theme color */}
-      <Flex bg="whitesmoke" padding={{ base: 2, md: 4 }} justifyContent="center" fontSize={{ base: "xs", md: "sm" }}>
-        <Flex 
-          justifyContent="space-between" 
+      <Flex
+        bg="whitesmoke"
+        padding={{ base: 2, md: 4 }}
+        justifyContent="center"
+        fontSize={{ base: "xs", md: "sm" }}
+      >
+        <Flex
+          justifyContent="space-between"
           width={{ base: "95vw", md: "60vw" }}
           flexDirection={{ base: "column", md: "row" }}
           alignItems="center"
           gap={{ base: 2, md: 0 }}
         >
-          <Box textAlign="center">© {year} State Restorer. All rights reserved.</Box>
-          <Flex spaceX={{ base: 2, md: 4 }} flexWrap="wrap" justifyContent="center">
+          <Box textAlign="center">
+            © {year} State Restorer. All rights reserved.
+          </Box>
+          <Flex
+            spaceX={{ base: 2, md: 4 }}
+            flexWrap="wrap"
+            justifyContent="center"
+          >
             <Link href="/privacy">Privacy Policy</Link>
             <Link href="/terms-and-conditions">Terms and Conditions</Link>
+            <Text opacity="0.5">v{app_version}</Text>
           </Flex>
         </Flex>
       </Flex>
